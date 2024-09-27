cf4e896a88189c8295f4f405d95c9f6f2a50e3a5
1081931 <1081931@lammersvilleusd.net>
2024-09-27 10:23:03 -0700

    Update CSP Project 1.py

--- CSP Project 1.py
+++ CSP Project 1.py
@@ -2,7 +2,7 @@
 import turtle as trtl
 x = trtl.Turtle()
 
-color_list = ["red","blue","yellow","grey","green"]
+color_list = ["red","blue","yellow","purple","green"]
 
 
 pensize = int(input("Enter a pen size"))
@@ -13,7 +13,8 @@ repeat = int(input("How many times do you want the pattern to repeat"))
 step_angle = int(input("What do you wnat the angle between each shape to be?"))
 
 for i in range(repeat):
-  x.color(color_list)
+  x.pencolor("black")
+  x.fillcolor(color_list[i%len(color_list)])
   x.begin_fill()
   x.circle(radius,360,step)
   x.end_fill()
