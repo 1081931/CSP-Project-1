a401d93fa1c79a3e78b1d1ac3add5489be3e1609
1081931 <1081931@lammersvilleusd.net>
2024-09-27 10:04:27 -0700

    Update CSP Project 1.py

--- CSP Project 1.py
+++ CSP Project 1.py
@@ -1,12 +1,12 @@
 # Project
 import turtle as trtl
 x = trtl.Turtle()
-x.pensize(30)
-i=int(input("Enter the length of the star"))
-z=int(input("Enter the angle"))
+x.pensize(10)
+radius = int(input("Enter the radius of the circle"))
+step = int(input("Enter the amount of steps that you want"))
 for i in range(4):
-  x.forward(i)
-  x.right(120)
+
+  x.circle(radius,360,step100)
 wn = trtl.Screen()
 wn.mainloop()
 #idk
