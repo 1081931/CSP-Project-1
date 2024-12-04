6062604e7396d876c003712c4af557ad3dce7bf0
1081931 <1081931@lammersvilleusd.net>
2024-12-04 14:45:24 -0800

    Update CSP PLTW UNIT-2 Mastery Project 2 - TKinter Color Guessing Game.py

--- CSP PLTW UNIT-2 Mastery Project 2 - TKinter Color Guessing Game.py
+++ CSP PLTW UNIT-2 Mastery Project 2 - TKinter Color Guessing Game.py
@@ -1,45 +1,56 @@
 import tkinter as tk
+import random
+from turtle import onclick
 root = tk.Tk()
 root.wm_geometry("400x600")
 
 
 frame_color = tk.Frame(root)
 
-canvas = tk.Canvas(root, width = 300, height = 200, background = "red")
-canvas.pack()
 
-canvas.config(ANCHOR=tk.CENTER)
+colors = ["red","green","blue","yellow","orange", "purple", "pink", "brown", "black", "white"]
+variable = random.choice(colors)
+
+def button(color):
+    if color == variable:
+        print("congrats")
+    else:
+        print("u are dumdum")
+
+canvas = tk.Canvas(root, width = 300, height = 200, background = variable)
+canvas.pack()
 
 canvas.create_text(50, 50, text = "hi", font = ("arial", 30))
 
-red = tk.Button(root, text = "red", command  = "onclick", background = "red")
+red = tk.Button(root, text = "red", background = "red", command = lambda: button("red"))
 red.pack()
 
-green = tk.Button(root, text = "green", command  = "onclick", background = "green")
+green = tk.Button(root, text = "green", background = "green", command = lambda: button("green"))
 green.pack()
 
-blue = tk.Button(root, text = "blue", command  = "onclick", background = "blue")
+blue = tk.Button(root, text = "blue", background = "blue", command = lambda: button("blue"))
 blue.pack()
 
-yellow = tk.Button(root, text = "yellow", command  = "onclick", background = "yellow")
+yellow = tk.Button(root, text = "yellow", background = "yellow", command = lambda: button("yellow"))
 yellow.pack()
 
-orange = tk.Button(root, text = "orange", command  = "onclick", background = "orange")
+orange = tk.Button(root, text = "orange", background = "orange", command = lambda: button("orange"))
 orange.pack()
 
-purple = tk.Button(root, text = "purple", command  = "onclick", background = "purple")
+purple = tk.Button(root, text = "purple", background = "purple", command = lambda: button("purple"))
 purple.pack()
 
-pink = tk.Button(root, text = "pink", command  = "onclick", background = "pink")
+pink = tk.Button(root, text = "pink", background = "pink", command = lambda: button("pink"))
 pink.pack()
 
-brown = tk.Button(root, text = "brown", command  = "onclick", background = "brown")
+brown = tk.Button(root, text = "brown", background = "brown", command = lambda: button("brown"))
 brown.pack()
 
-black = tk.Button(root, text = "black", command  = "onclick", background = "black")
+black = tk.Button(root, text = "black", background = "black", command = lambda: button("black"))
 black.pack()
 
-white = tk.Button(root, text = "white", command  = "onclick", background = "white")
+white = tk.Button(root, text = "white", background = "white", command = lambda: button("white"))
 white.pack()
+
 
 root.mainloop()
